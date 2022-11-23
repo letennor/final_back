219092cdb94774cbe07ea51ab62953eb65945967
letennor <h1229523967@163.com>
2022-11-23 12:17:44 +0800

    实现添加死亡记录

---
+++ src/main/java/com/final_back/controller/DeathRecordController.java
@@ -0,0 +1,29 @@
+package com.final_back.controller;
+
+import com.final_back.entity.DeathRecord;
+import com.final_back.mapper.DeathRecordMapper;
+import com.final_back.utils.result.Result;
+import com.final_back.utils.result.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeathRecordController {
+
+    @Autowired
+    DeathRecordMapper deathRecordMapper;
+
+    @RequestMapping("/addDeathRecord")
+    public Result<?> addDeathRecord(@RequestBody DeathRecord deathRecord){
+        int insert = deathRecordMapper.insert(deathRecord);
+        if (insert > 0){
+            return ResultUtil.success("插入成功");
+        }else {
+            return ResultUtil.success("插入失败");
+        }
+
+    }
+
+}
---
+++ src/main/java/com/final_back/mapper/DeathRecordMapper.java
@@ -0,0 +1,9 @@
+package com.final_back.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.final_back.entity.DeathRecord;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeathRecordMapper extends BaseMapper<DeathRecord> {
+}
